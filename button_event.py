f09b1a629194959e0d270eb2c95cbb316954a4c6
Barry Tarlton <TARLTOB1@nationwide.com>
2017-10-21 13:42:47 -0400

    initial commit with code for button event

---
+++ button_event.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+import paho.mqtt.client as mqtt
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+        input_state = GPIO.input(18)
+        if input_state == False:
+                print('Button Pressed')
+                broadcastEvent("buttonPressed")
+                time.sleep(1) # wait a second before we listen for more button presses
+
+
+
+
+
+def broadcastEvent(event):
+    mqttc = mqtt.Client("buttonMachine")
+    #mqttc.connect("10.0.0.1", 1883)
+    mqttc.connect("localhost", 1883)
+    # don't retain messages, this needs to be more real -time
+    mqttc.publish("events/button", payload=event,qos=0,retain=False) 
+    mqttc.loop(2) #timeout = 2s
+
